243fc0abfa97ea096fe9ea691883b8d09e773507
DimaChesnokov <100202160+DimaChesnokov@users.noreply.github.com>
2024-12-02 18:47:45 +0300

    WatgerFallProject

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEngineCapacity = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReleaseDate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.btnAddClient = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.txtClientName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContactInfo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDesiredBrand = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDesiredEngineCapacity = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSearchBrand = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSearchEngineCapacity = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.клиентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.машиныToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заявкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(12, 50);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(100, 22);
+            this.txtBrand.TabIndex = 0;
+            this.txtBrand.TextChanged += new System.EventHandler(this.txtBrand_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Бренд";
+            // 
+            // txtEngineCapacity
+            // 
+            this.txtEngineCapacity.Location = new System.Drawing.Point(132, 50);
+            this.txtEngineCapacity.Name = "txtEngineCapacity";
+            this.txtEngineCapacity.Size = new System.Drawing.Size(100, 22);
+            this.txtEngineCapacity.TabIndex = 2;
+            this.txtEngineCapacity.TextChanged += new System.EventHandler(this.txtEngineCapacity_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Мощность двигателя";
+            // 
+            // txtReleaseDate
+            // 
+            this.txtReleaseDate.Location = new System.Drawing.Point(289, 50);
+            this.txtReleaseDate.Name = "txtReleaseDate";
+            this.txtReleaseDate.Size = new System.Drawing.Size(100, 22);
+            this.txtReleaseDate.TabIndex = 4;
+            this.txtReleaseDate.TextChanged += new System.EventHandler(this.txtReleaseDate_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Дата выпуска";
+            // 
+            // btnAddCar
+            // 
+            this.btnAddCar.Location = new System.Drawing.Point(411, 50);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(129, 23);
+            this.btnAddCar.TabIndex = 6;
+            this.btnAddCar.Text = "ДобавитьАвто";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            this.btnAddCar.Click += new System.EventHandler(this.btnAddCar_Click);
+            // 
+            // btnAddClient
+            // 
+            this.btnAddClient.Location = new System.Drawing.Point(553, 111);
+            this.btnAddClient.Name = "btnAddClient";
+            this.btnAddClient.Size = new System.Drawing.Size(176, 23);
+            this.btnAddClient.TabIndex = 7;
+            this.btnAddClient.Text = "ДобавитьКлиент";
+            this.btnAddClient.UseVisualStyleBackColor = true;
+            this.btnAddClient.Click += new System.EventHandler(this.btnAddClient_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(245, 170);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(192, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "ПоискМашины";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.ItemHeight = 16;
+            this.lstResults.Location = new System.Drawing.Point(829, 84);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(573, 68);
+            this.lstResults.TabIndex = 9;
+            // 
+            // txtClientName
+            // 
+            this.txtClientName.Location = new System.Drawing.Point(12, 111);
+            this.txtClientName.Name = "txtClientName";
+            this.txtClientName.Size = new System.Drawing.Size(100, 22);
+            this.txtClientName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Клиент";
+            // 
+            // txtContactInfo
+            // 
+            this.txtContactInfo.Location = new System.Drawing.Point(132, 111);
+            this.txtContactInfo.Name = "txtContactInfo";
+            this.txtContactInfo.Size = new System.Drawing.Size(100, 22);
+            this.txtContactInfo.TabIndex = 12;
+            this.txtContactInfo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Контактная информация";
+            // 
+            // txtDesiredBrand
+            // 
+            this.txtDesiredBrand.Location = new System.Drawing.Point(307, 111);
+            this.txtDesiredBrand.Name = "txtDesiredBrand";
+            this.txtDesiredBrand.Size = new System.Drawing.Size(100, 22);
+            this.txtDesiredBrand.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(304, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "МаркаНужная";
+            // 
+            // txtDesiredEngineCapacity
+            // 
+            this.txtDesiredEngineCapacity.Location = new System.Drawing.Point(429, 111);
+            this.txtDesiredEngineCapacity.Name = "txtDesiredEngineCapacity";
+            this.txtDesiredEngineCapacity.Size = new System.Drawing.Size(100, 22);
+            this.txtDesiredEngineCapacity.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(430, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "ДвигательНужный";
+            // 
+            // txtSearchBrand
+            // 
+            this.txtSearchBrand.Location = new System.Drawing.Point(11, 171);
+            this.txtSearchBrand.Name = "txtSearchBrand";
+            this.txtSearchBrand.Size = new System.Drawing.Size(100, 22);
+            this.txtSearchBrand.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 16);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "ПоискПоМарке";
+            // 
+            // txtSearchEngineCapacity
+            // 
+            this.txtSearchEngineCapacity.Location = new System.Drawing.Point(132, 171);
+            this.txtSearchEngineCapacity.Name = "txtSearchEngineCapacity";
+            this.txtSearchEngineCapacity.Size = new System.Drawing.Size(100, 22);
+            this.txtSearchEngineCapacity.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(129, 142);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 16);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "ПоискПоДвигателю";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1022, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Заявка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 212);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1481, 371);
+            this.dataGridView1.TabIndex = 23;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.клиентыToolStripMenuItem,
+            this.машиныToolStripMenuItem,
+            this.заявкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1508, 28);
+            this.menuStrip1.TabIndex = 24;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // клиентыToolStripMenuItem
+            // 
+            this.клиентыToolStripMenuItem.Name = "клиентыToolStripMenuItem";
+            this.клиентыToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.клиентыToolStripMenuItem.Text = "Клиенты";
+            this.клиентыToolStripMenuItem.Click += new System.EventHandler(this.клиентыToolStripMenuItem_Click);
+            // 
+            // машиныToolStripMenuItem
+            // 
+            this.машиныToolStripMenuItem.Name = "машиныToolStripMenuItem";
+            this.машиныToolStripMenuItem.Size = new System.Drawing.Size(85, 24);
+            this.машиныToolStripMenuItem.Text = "Машины";
+            this.машиныToolStripMenuItem.Click += new System.EventHandler(this.машиныToolStripMenuItem_Click);
+            // 
+            // заявкиToolStripMenuItem
+            // 
+            this.заявкиToolStripMenuItem.Name = "заявкиToolStripMenuItem";
+            this.заявкиToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.заявкиToolStripMenuItem.Text = "Заявки";
+            this.заявкиToolStripMenuItem.Click += new System.EventHandler(this.заявкиToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1508, 595);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtSearchEngineCapacity);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtSearchBrand);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDesiredEngineCapacity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDesiredBrand);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtContactInfo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtClientName);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAddClient);
+            this.Controls.Add(this.btnAddCar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtReleaseDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEngineCapacity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEngineCapacity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReleaseDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.Button btnAddClient;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.TextBox txtClientName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContactInfo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDesiredBrand;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDesiredEngineCapacity;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSearchBrand;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSearchEngineCapacity;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem клиентыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem машиныToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заявкиToolStripMenuItem;
+    }
+}
+
